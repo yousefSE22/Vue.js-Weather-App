ce32f05d98ceb5c93b14b038c5373a552f161947
Youssef Younes <yy222bz@student.lnu.se>
2024-09-15 17:37:16 +0200

    Added search form

--- WeatherApp/src/App.vue
+++ WeatherApp/src/App.vue
@@ -1,5 +1,25 @@
+<script setup>
+import SearchInput from './components/SearchInput.vue'
+
+</script>
+
 <template>
   <main>
-    Home
+    <!-- Date of the day -->
+    <div class="text-center mb-6">
+      {{
+        new Date().toLocaleDateString('en-US', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        })
+      }}
+    </div>
+
+    <!-- search field -->
+    <div>
+      <SearchInput/>
+    </div>
   </main>
 </template>
